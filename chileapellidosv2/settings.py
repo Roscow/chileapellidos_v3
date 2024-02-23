a8d5dc96c2d43da96d4f96294de2985ffc47e9c1
nicolas valdes <nicolas.valdes@live.com>
2024-02-23 14:07:02 -0300

    config gunicorn, allowed host

--- chileapellidosv2/settings.py
+++ chileapellidosv2/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = db_host = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','nicolasvaldes.tech', ]
 
 
 # Application definition
