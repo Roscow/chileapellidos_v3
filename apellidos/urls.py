b0f97580afb2a61a86069cc46960371518d9ce83
Nicolas valdes <nicolas.valdes@live.com>
2024-09-18 23:10:23 -0300

    desactivar get detalle apellido

--- apellidos/urls.py
+++ apellidos/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('ranking_dif_genero_desc/<int:pagina>', views.ranking_dif_genero_desc, name='ranking_dif_genero_desc'),  
     path('ranking_mas_buscados_asc/<int:pagina>', views.ranking_mas_buscados_asc, name='ranking_mas_buscados_asc'),  
     path('ranking_mas_buscados_desc/<int:pagina>', views.ranking_mas_buscados_desc, name='ranking_mas_buscados_desc'), 
-    path('detalle_apellido2/<str:apellido>', views.detalle_apellido2, name='detalle_apellido2'),  
+    #path('detalle_apellido2/<str:apellido>', views.detalle_apellido2, name='detalle_apellido2'),  
     path('solicitud_revision', views.solicitud_revision, name='solicitud_revision'),  
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
